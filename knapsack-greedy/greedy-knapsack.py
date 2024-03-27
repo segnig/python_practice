d3072427ef2218a381b99f8ec59a5d5878138406
Segni Girma <106369427+segnig@users.noreply.github.com>
2024-03-27 15:26:19 +0300

    Update greedy-knapsack.py

--- knapsack-greedy/greedy-knapsack.py
+++ knapsack-greedy/greedy-knapsack.py
@@ -1,16 +1,5 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Mar 26 13:30:30 2024
-
-@author: segni
-"""
-
 
 class Food(object):
-    """
-    Food class provides information about a particular food item
-    name, value, and calories 
-    """
     def __init__(self, name, value, calories):
         self.name = name
         self.calories = calories
@@ -25,41 +14,26 @@ class Food(object):
     def density(self):
         return self.getValue() / self.getCost()
     
+    
     def __str__(self):
         return self.name + ": < " + str(self.value) + ",  " + str(self.calories) + '>'
     
     
-def buildMenu(names, values, calories):
-    """
-    name, values, calories lists of the same 
-    length name a list of strings values and 
-    calories lists of numbers return list of Foods
-    
-    """
+def buildMenu(names:list, values:list, calories:list):
     menu = []
-    
-    
-    
-    for i in range(len(values)):
+    for i in range(len(names)):
         food = Food(names[i], values[i], calories[i])
         menu.append(food)
-                    
     return menu
 
-
 def greedy(items, maxCost, keyFunction):
-    """
-    Assume items a list , maxCost >= 0 
-    keyFunction maps elements of items to numbers
-    """
-    
     itemsCopy = sorted(items, key=keyFunction, reverse=True)
     result = []
-    totalValue, totalCost = 0.0, 0.0 
+    totalValue, totalCost = 0, 0.0
     
     for i in range(len(itemsCopy)):
         if totalCost + itemsCopy[i].getCost() <= maxCost:
-            result.append(itemsCopy)
+            result.append(itemsCopy[i])
             totalCost += itemsCopy[i].getCost()
             totalValue += itemsCopy[i].getValue()
             
@@ -71,7 +45,7 @@ def testGreedy(items, constraints, keyFunction):
     
     print(f"Total vales of items taken = {val}")
     for item in items:
-        print("   ", item)
+        print("    ", item)
     
 
 def testGreedys(foods, maxUnits):
@@ -86,11 +60,50 @@ def testGreedys(foods, maxUnits):
     
     testGreedy(foods, maxUnits, Food.density)
 
+def maxVal(toConsider, avail):
+    """
+    Assume toCosider a list of items, avail a weight 
+    Return  a tuple of the total value of a solution 
+    to the 0/1 knapsack problem and the items of that solution
+    """
+    if toConsider == [] or avail == 0:
+        result = (0, ())
+    elif toConsider[0].getCost() > avail:
+        # Explore right branch only
+        result = maxVal(toConsider[1:], avail)
+        
+    else:
+        nextItem = toConsider[0]
+        # Explore left branch 
+        withVal, withToTake = maxVal(toConsider[1:], avail - nextItem.getCost())
+        
+        withVal += nextItem.getValue()
+        # Exploree right branch
+        withoutVal, withoutToTake = maxVal(toConsider[1:], avail)
+        # Choose better branch
+        if withVal > withoutVal:
+            result = (withVal, withToTake + (nextItem,))
+        else:
+            result = (withoutVal, withoutToTake)
+        
+    return result
+
+def testMaxVal(foods, maxUnits, printItems=True):
+    print("Use search tree to allocate", maxUnits, "calories")
+    
+    val, taken = maxVal(foods, maxUnits)
+    print("Total value of items taken =", val)
+    if printItems:
+        for item in taken:
+            print ("    ", item)
+
 if __name__ == "__main__":      
     names = ["wine", "beer", "pizza", "burger", "fries",
             "cola", "apple", "donut", "cake"]
-    values = [89, 90, 95, 100, 90, 79, 50, 10]
-    calories = [123, 154, 258, 354, 365, 150, 95, 195]
+    values = [89, 90, 95, 100, 90, 79, 50, 10, 12]
+    calories = [123, 154, 258, 354, 365, 150, 95, 195, 123]
     foods = buildMenu(names, values, calories)
-
     testGreedys(foods, 750)
+    print("************")
+    testMaxVal(foods, 750)
+        
